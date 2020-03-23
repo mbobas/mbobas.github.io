628cd4576e69ce8496fd335aa965a5c113ddd762
mbobas <michal.gulczynski88@gmail.com>
2020-03-23 19:43:10 +0100

    Updates

---
+++ precache-manifest.acd69c2168c0428c0bae7b356223da47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f6080f508a070d8e84f6df4d7948f17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20c953c6c6f1db5a4e76",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "14315ef22a1bc44b1091",
+    "url": "/static/js/2.b6bc716c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b6bc716c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20c953c6c6f1db5a4e76",
+    "url": "/static/js/main.01990f29.chunk.js"
+  },
+  {
+    "revision": "dedab4d81692fc420873",
+    "url": "/static/js/runtime-main.bec56e5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
